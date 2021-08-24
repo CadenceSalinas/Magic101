a351397c0c8d4fb6c337c27db026c3593dc05322
Cadence <cadence242@roadrunner.com>
2021-08-24 08:25:31 -0700

    fixed castSpell()

--- src/Player.java
+++ src/Player.java
@@ -42,7 +42,7 @@ public class Player
         if(mp>0)
         {
             System.out.println("Abracadabra alakazam!");
-            mp =- 5;
+            mp -= 5;
         }
         else
         {
@@ -51,7 +51,7 @@ public class Player
     }
     public void loseHealth()
     {
-        if(this.health >= 0)
+        if(health >= 0)
         {
             System.out.println(name + " took some damage!");
             health -= 10;
