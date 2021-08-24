cf890c68174aecdca166d993c3f7170f56d61587
Cadence <cadence242@roadrunner.com>
2021-08-24 08:39:39 -0700

    added static variable to MagicDriver()

--- src/MagicDriver.java
+++ src/MagicDriver.java
@@ -2,8 +2,10 @@ import java.sql.SQLOutput;
 
 public class MagicDriver
 {
+    private static int count;
     public static void main(String[] args)
     {
+        //Enemies
         Goblin kyle = new Goblin( 100,true, 5);
         Goblin gerome = new Goblin (100, false, 0);
         Dragon erika = new Dragon(250, true, 10);
@@ -11,15 +13,18 @@ public class MagicDriver
         Golem issac = new Golem(300, false, Golem.Elements.air);
         Golem villageDefender = new Golem(300, false, Golem.Elements.iron);
 
+        //Player/sorcerer
         Player kenzu = new Player(200, 100, "kenzu");
 
-        // System.out.println(kenzu.toString());
+        System.out.println(kenzu.toString()+"\n");
         Enemies[] myEnemies = {kyle, gerome, erika, stuntedBird, issac, villageDefender};
 
-//        for(int i = 0; i < myEnemies.length; i++)
-//        {
-//            System.out.println(myEnemies[i]);
-//        }
+        for(int i = 0; i < myEnemies.length; i++)
+        {
+            System.out.println(myEnemies[i]);
+            count++;
+        }
+        System.out.println("The count is " + count);
 
     }
 }//end driver
